d294d4eec83a5217c197dd62c4aeb75c90b46f83
Arkadiusz Chmura <arekchmura@gmail.com>
2024-12-02 16:35:27 +0100

    Refactor

--- src/main/kotlin/com/clouddjr/advent2024/Day02.kt
+++ src/main/kotlin/com/clouddjr/advent2024/Day02.kt
@@ -3,14 +3,18 @@ package com.clouddjr.advent2024
 class Day02(input: List<String>) {
     private val reports = input.map { report -> report.split(" ").map { it.toInt() } }
 
-    fun solvePart1() = reports.toDiffs().count { diff -> diff.isSafe() }
+    fun solvePart1() = reports.count { isSafe(it) }
 
-    fun solvePart2() = reports.count { original ->
-        original.indices.map { toRemove -> original.filterIndexed { i, _ -> i != toRemove } }
-            .toDiffs().any { diff -> diff.isSafe() }
+    fun solvePart2() = reports.count { isSafeWithCorrection(it) }
+
+    private fun isSafe(report: List<Int>): Boolean {
+        val diffs = report.zipWithNext { a, b -> b - a }
+        return diffs.all { it in 1..3 } || diffs.all { it in -3..-1 }
     }
 
-    private fun List<List<Int>>.toDiffs() = map { it.zipWithNext().map { (a, b) -> b - a } }
-
-    private fun List<Int>.isSafe() = all { it in 1..3 } || all { it in -3..-1 }
+    private fun isSafeWithCorrection(report: List<Int>): Boolean {
+        return report.indices
+            .map { toRemove -> report.filterIndexed { i, _ -> i != toRemove } }
+            .any { isSafe(it) }
+    }
 }
