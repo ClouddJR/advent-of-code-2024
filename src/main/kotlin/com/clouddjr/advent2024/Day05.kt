f24f510a123ba114606ed420ca2dad3525c5f721
Arkadiusz Chmura <arekchmura@gmail.com>
2024-12-07 17:13:06 +0100

    Change formatting

--- src/main/kotlin/com/clouddjr/advent2024/Day05.kt
+++ src/main/kotlin/com/clouddjr/advent2024/Day05.kt
@@ -9,11 +9,20 @@ class Day05(input: String) {
 
     private val updates = input.substringAfter("\n\n").lines().map { it.split(",").map(String::toInt) }
 
-    fun solvePart1() = updates.filter { it.isSortedByRules() }.sumOf { it[it.size / 2] }
+    fun solvePart1() =
+        updates
+            .filter { it.isSortedByRules() }
+            .sumOf { it[it.size / 2] }
 
-    fun solvePart2() = updates.filterNot { it.isSortedByRules() }.map { it.sortedByRules() }.sumOf { it[it.size / 2] }
+    fun solvePart2() =
+        updates
+            .filterNot { it.isSortedByRules() }
+            .map { it.sortedByRules() }
+            .sumOf { it[it.size / 2] }
 
     private fun List<Int>.isSortedByRules() = this == this.sortedByRules()
 
-    private fun List<Int>.sortedByRules() = this.sortedWith { o1, o2 -> if (o2 in rules.getValue(o1)) -1 else 1 }
+    private fun List<Int>.sortedByRules() = this.sortedWith { o1, o2 ->
+        if (o2 in rules.getValue(o1)) -1 else 1
+    }
 }
